8c0c6dc51c8d33d8774c03027f866fd768967d38
Yousaf Awais <yousafawais987@gmail.com>
2026-07-25 06:02:11 +0500

    ok live

--- src/app.js
+++ src/app.js
@@ -21,6 +21,7 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:5174',
   'http://localhost:5175',
+  'https://car-parking-lime.vercel.app',
   'https://noor-rista-admin.vercel.app',
   'https://noor-rista-dashboard.vercel.app',
 ];
